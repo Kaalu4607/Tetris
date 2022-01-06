9afc51fff921b52a3a4148221b2da12b97eb07a0
Kaalu4607 <s351315399@gmail.com>
2022-01-06 23:47:17 +0800

    Update playColor.py

--- playColor.py
+++ playColor.py
@@ -677,14 +677,14 @@ while running:
             #-------------------------------------------------------------########
             #刪除方塊(儲存的)
             elif event.key == pygame.K_LSHIFT and game_mode == 0:    
-                 brick_next_next_id = random.randint(1, 7)
+                 brick_next_next_id = brick_id 
                  brick_state = 0 #清空還在跑的方塊
                  #設定初始位置
                  container_x = 3
                  container_y =-4
                 # 現在出現方塊.
-                 brick_id = brick_next_id 
-                 brick_next_id = random.randint(1, 7)#編號1~7
+                 brick_id = brick_next_id
+                 brick_next_id = random.randint(1, 7)
                 
         #-----------------------------------------------------------------
         # 判斷放開按鈕
